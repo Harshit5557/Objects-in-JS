95b3f606e340bd56006aa9646f55433d55450804
Harshit5557 <akhilendra054@gmail.com>
2024-09-09 18:42:20 +0530

    Initial Commit

---
+++ Maths.js
@@ -0,0 +1,17 @@
+var value=Math.acos(-1)
+console.log("Result 1=",+value,+"<br>");
+
+var value=Math.asin(1)
+console.log("Result 2=",+value,+"<br>");
+
+var value=Math.atan(.5)
+console.log("Result 3=",+value,+"<br>");
+
+var value=Math.random()
+console.log(value);
+
+var value=Math.sqrt(4)
+console.log(value);
+
+var value=Math.pow(4,456478)
+console.log(value);
---
+++ Object.js
@@ -0,0 +1,8 @@
+var data={
+    name: "Donald R",
+    gender: "male",
+    race: "White",
+    age: "54",
+};
+
+console.log(data.name,data.gender,data.race,data.age);
